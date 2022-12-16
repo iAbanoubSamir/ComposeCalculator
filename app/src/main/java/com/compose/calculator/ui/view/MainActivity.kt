4809910f72fefd23dea72a2518cc181ac43d04c7
Abanoub Samir <abanoub.sameer.22@gmail.com>
2022-12-16 18:30:36 +0200

    Setup Screen and ViewModel

--- app/src/main/java/com/compose/calculator/ui/view/MainActivity.kt
+++ app/src/main/java/com/compose/calculator/ui/view/MainActivity.kt
@@ -3,21 +3,32 @@ package com.compose.calculator.ui.view
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.compose.calculator.ui.theme.ComposeCalculatorTheme
+import com.compose.calculator.ui.theme.MediumGray
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeCalculatorTheme {
-
+                val viewModel = viewModel<MainViewModel>()
+                val state = viewModel.state
+                val buttonSpacing = 8.dp
+                Calculator(
+                    state = state,
+                    onAction = viewModel::onAction,
+                    buttonSpacing = buttonSpacing,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MediumGray)
+                        .padding(16.dp)
+                )
             }
         }
     }
